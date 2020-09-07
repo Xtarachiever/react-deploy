be708f4fa05e1aa37a0bce1bbeae96bae64e2cca
Adeyemi Esther <63353240+Xtarachiever@users.noreply.github.com>
2020-09-07 08:27:32 +0100

    Updates

---
+++ precache-manifest.13c8cf6148fd318dc3a04cb092ccf779.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c9265d4b7e4a2820ff9b7f322feafe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132987688c83c902bda2",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "ca7df1a719b8552a8570",
+    "url": "/static/css/main.d6bcb662.chunk.css"
+  },
+  {
+    "revision": "132987688c83c902bda2",
+    "url": "/static/js/2.a30e128b.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.a30e128b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7df1a719b8552a8570",
+    "url": "/static/js/main.789b0703.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ precache-manifest.efb97030b75ef0473b81559c9e6dcc55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5305896e23c78cc76fd89a711b75ea2",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "9b28123750f6b8caf987",
+    "url": "/react-deploy/static/css/2.01b1f53e.chunk.css"
+  },
+  {
+    "revision": "e11b6b5cb691b58f9930",
+    "url": "/react-deploy/static/css/main.d6bcb662.chunk.css"
+  },
+  {
+    "revision": "9b28123750f6b8caf987",
+    "url": "/react-deploy/static/js/2.a30e128b.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react-deploy/static/js/2.a30e128b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11b6b5cb691b58f9930",
+    "url": "/react-deploy/static/js/main.74a1caea.chunk.js"
+  },
+  {
+    "revision": "a07c0a8e1fb3f51fcb56",
+    "url": "/react-deploy/static/js/runtime-main.a2e67403.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-deploy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-deploy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-deploy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-deploy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-deploy/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
